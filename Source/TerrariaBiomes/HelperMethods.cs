0aea65e6b863266da48709679a18efd2db294678
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-05-11 13:12:50 -0600

    Corruption subbiomes have appropriate plant and animal densities

---
+++ Source/TerrariaBiomes/HelperMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TerrariaBiomes
+{
+    public static class HelperMethods
+    {
+        public static float GetDensityMultiplier(Map map)
+        {
+            float factor = 1f;
+            if (map.TileInfo.temperature < -25f)
+            {
+                factor *= 0.1f;
+            }
+            else if (map.TileInfo.temperature < -15f)
+            {
+                factor *= 0.3f;
+            }
+            else if (map.TileInfo.temperature < -5f)
+            {
+                factor *= 0.75f;
+            }
+            else if (map.TileInfo.rainfall < 600f)
+            {
+                factor *= 0.15f;
+            }
+            else if (map.TileInfo.temperature > 18)
+            {
+                if (map.TileInfo.rainfall > 2200)
+                {
+                    factor *= 1.5f;
+                }
+                else
+                {
+                    factor *= 0.5f;
+                }
+            }
+
+            if (map.TileInfo.swampiness > 0.5f)
+            {
+                factor *= 1.5f;
+            }
+
+            return factor;
+        }
+    }
+}
---
+++ Source/TerrariaBiomes/LifeDensityPatches.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaBiomes
+{
+    [HarmonyPatch(typeof(WildAnimalSpawner), "DesiredTotalAnimalWeight", MethodType.Getter)]
+    static class AnimalDensityPatch
+    {
+        static void Postfix(Map ___map, ref float __result)
+        {
+            if (___map.Biome.defName != "ZTB_Corruption")
+            {
+                return;
+            }
+            __result *= HelperMethods.GetDensityMultiplier(___map);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(WildPlantSpawner), "CurrentPlantDensity", MethodType.Getter)]
+    static class PlantDensityPatch
+    {
+        static void Postfix(Map ___map, ref float __result)
+        {
+            if(___map.Biome.defName != "ZTB_Corruption")
+            {
+                return;
+            }
+            __result *= HelperMethods.GetDensityMultiplier(___map);
+        }
+    }
+
+
+}
