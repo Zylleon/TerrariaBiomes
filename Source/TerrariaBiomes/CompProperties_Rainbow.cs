f539eff568e1c3d1a3da7e7c9df2508591a78654
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-05-22 14:19:52 -0600

    rainbow slime

---
+++ Source/TerrariaBiomes/CompProperties_Rainbow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TerrariaBiomes
+{
+    public class CompProperties_Rainbow : CompProperties
+    {
+        public CompProperties_Rainbow()
+        {
+            compClass = typeof(CompRainbow);
+        }
+    }
+
+
+
+    public class CompRainbow : ThingComp
+    {
+        public override void CompTick()
+        {
+            Pawn pawn = parent as Pawn;
+            pawn.Drawer.renderer.graphics.ResolveAllGraphics();
+        }
+
+    }
+}
---
+++ Source/TerrariaBiomes/ZTB_DefOf.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TerrariaBiomes
+{
+    [DefOf]
+    public static class ZTB_DefOf
+    {
+        public static BiomeDef ZTB_Corruption;
+        public static BiomeDef ZTB_Hallow;
+
+        public static PawnKindDef ZTB_RainbowSlime;
+    }
+
+}
